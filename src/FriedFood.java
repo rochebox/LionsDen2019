2c09c4a04cf005074aa294d86df74c84133960bf
Sarah <SarahLammert@stmarksschool.org>
2019-10-25 09:03:01 -0400

    1st Commit

---
+++ src/FriedFood.java
@@ -0,0 +1,4 @@
+
+public class FriedFood extends FoodItem {
+
+}
--- src/LionDenRunner.java
+++ src/LionDenRunner.java
@@ -2,8 +2,7 @@
 public class LionDenRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
